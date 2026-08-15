f4cee0093456a726a5fdd53c7a961666b70ddfa1
Mark Waichigo <124110345+markie-maker@users.noreply.github.com>
2026-08-15 01:08:44 -0700

    Addwd New Sections

---
+++ src/components/goal-accordian.svelte
@@ -0,0 +1,26 @@
+<script>
+  let isOpen = $state(false);
+
+  const { title, description } = $props()
+</script>
+
+<div class={`overflow-hidden rounded-2xl bg-white transition-transform duration-300 ease-in-out ${isOpen ? '' : 'active:scale-95'}`}>
+  <button
+    onclick={() => (isOpen = !isOpen)}
+    aria-expanded={isOpen}
+    class="cursor-pointer active:scale-95 appearance-none flex w-full items-center justify-between bg-white p-4 text-left transition-colors duration-300 ease-in-out"
+  >
+    <p class="light text-xl">{title}</p>
+    <svg xmlns="http://www.w3.org/2000/svg" class={`transition-transform duration-300 ease-in-out ${isOpen ? 'rotate-90' : '-rotate-90'}`} height="40px" viewBox="0 -960 960 960" width="40px" fill="#222222"><path d="M522.67-480.67 331.33-672 376-717.33l236.67 236.66L376-244l-44.67-45.33 191.34-191.34Z"/></svg>
+</button>
+
+  <div class={`grid transition-[grid-template-rows] duration-300 ease-in-out ${isOpen ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'}`}>
+    <div class="overflow-hidden">
+      <div class="bg-white px-4 pb-4">
+        <p class="text-xl light">
+          {description}
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
