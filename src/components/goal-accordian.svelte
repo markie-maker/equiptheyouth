62b58579d1566ed4b529b5c8202d4e29705e5a71
Mark Waichigo <124110345+markie-maker@users.noreply.github.com>
2026-08-15 16:36:39 -0700

    Changed website icon and added/completed two pages
    Changed the icon to a zoomed in version and touched up the goal-accordian svelte component. Added a Donate Now page and also fixed the image component.

--- src/components/goal-accordian.svelte
+++ src/components/goal-accordian.svelte
@@ -11,13 +11,13 @@
     class="cursor-pointer active:scale-95 appearance-none flex w-full items-center justify-between bg-white p-4 text-left transition-colors duration-300 ease-in-out"
   >
     <p class="light text-xl">{title}</p>
-    <svg xmlns="http://www.w3.org/2000/svg" class={`transition-transform duration-300 ease-in-out ${isOpen ? 'rotate-90' : '-rotate-90'}`} height="40px" viewBox="0 -960 960 960" width="40px" fill="#222222"><path d="M522.67-480.67 331.33-672 376-717.33l236.67 236.66L376-244l-44.67-45.33 191.34-191.34Z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" class={`transition-transform duration-300 ease-in-out ${isOpen ? '-rotate-90' : 'rotate-90'}`} height="40px" viewBox="0 -960 960 960" width="40px" fill="#222222"><path d="M522.67-480.67 331.33-672 376-717.33l236.67 236.66L376-244l-44.67-45.33 191.34-191.34Z"/></svg>
 </button>
 
   <div class={`grid transition-[grid-template-rows] duration-300 ease-in-out ${isOpen ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'}`}>
     <div class="overflow-hidden">
       <div class="bg-white px-4 pb-4">
-        <p class="text-xl light">
+        <p class="text-lg">
           {description}
         </p>
       </div>
