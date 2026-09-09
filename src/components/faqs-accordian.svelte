6605108c2854cca4c1ce85cb8c977b604696f577
Mark Waichigo <124110345+markie-maker@users.noreply.github.com>
2026-09-08 21:46:21 -0700

    More text changes + taller header on xs devices

--- src/components/faqs-accordian.svelte
+++ src/components/faqs-accordian.svelte
@@ -11,7 +11,7 @@
     class="cursor-pointer appearance-none flex w-full items-center justify-between p-4 text-left transition-colors duration-300 ease-in-out"
   >
     <p class="light text-base md:text-xl text-[#222222]">{title}</p>
-    <svg xmlns="http://www.w3.org/2000/svg" class={`transition-transform duration-300 ease-in-out ${isOpen ? '-rotate-90' : 'rotate-90'}`} height="40px" viewBox="0 -960 960 960" width="40px" fill="#222222"><path d="M522.67-480.67 331.33-672 376-717.33l236.67 236.66L376-244l-44.67-45.33 191.34-191.34Z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" class={`shrink-0 transition-transform duration-300 ease-in-out ${isOpen ? '-rotate-90' : 'rotate-90'}`} height="40px" viewBox="0 -960 960 960" width="40px" fill="#222222"><path d="M522.67-480.67 331.33-672 376-717.33l236.67 236.66L376-244l-44.67-45.33 191.34-191.34Z"/></svg>
 </button>
 
   <div class={`grid transition-[grid-template-rows] duration-300 ease-in-out ${isOpen ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'}`}>
